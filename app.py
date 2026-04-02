054434c4cb775933731b697a135a6e6b12630f3a
Helohi <helomegido@gmail.com>
2026-04-02 15:53:11 +0300

    host changed

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from flask import Flask, render_template, send_from_directory
-import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/template.docx')
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/template.docx")
 def get_template():
     """Отдаем шаблон файла для загрузки в браузер"""
-    return send_from_directory('.', 'template.docx')
+    return send_from_directory(".", "template.docx")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=80)
